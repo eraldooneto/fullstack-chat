be5c1545ffdccd849f991164f33c2c31253cc6a9
eraldo <jesn@ic.ufal.br>
2023-11-19 00:47:28 -0300

    feat: chat ui complete

--- app/frontend/frontend/src/ChatsPage.jsx
+++ app/frontend/frontend/src/ChatsPage.jsx
@@ -1,5 +1,12 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic, useSingleChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('c8984229-a835-4bfd-801d-2ee522da0782', props.user.username, props.user.secret);
+
+    return <div style={{ height: '100vh' }}>
+        <MultiChatSocket {...chatProps}/>
+        <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+        </div>
 }
 
 export default ChatsPage
